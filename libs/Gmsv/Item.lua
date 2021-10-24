7f3bfbc2f187654f881229bea4e69b3efe62e1b6
Muscipular <m@ljw.im>
2021-10-24 16:57:43 +0800

    [+] Item.SetTimeLimit 设置道具限时 [+] Item.GetTimeLimit 道具限时

--- libs/Gmsv/Item.lua
+++ libs/Gmsv/Item.lua
@@ -97,3 +97,32 @@ ffi.hook.inlineHook('const char* (__cdecl *)(int itemIndex, int type, const char
   0x8B, 0x85, 0x10, 0x00, 0x00, 0x00, -- mov eax, [ebp + 0x10]
   0x89, 0x04, 0x24, -- arg itemIndex
 }, {})
+
+function Item.SetTimeLimit(CharIndex, ItemIndex, Time)
+  if Time < 0 then
+    Item.SetData(ItemIndex, 44, 0);
+    Item.SetData(ItemIndex, 45, 0);
+  else
+    Item.SetData(ItemIndex, 44, 1);
+    Item.SetData(ItemIndex, 44, Time + os.time());
+  end
+  local slot = Item.GetSlot(CharIndex, ItemIndex)
+  Item.UpItem(CharIndex, slot);
+end
+
+function Item.GetTimeLimit(CharIndex, ItemIndex)
+  local mode = Item.SetData(ItemIndex, 44);
+  local slot = Item.GetSlot(CharIndex, ItemIndex)
+  if slot < 0 then
+    return nil;
+  end
+  if mode == 2 then
+    Item.UpItem(CharIndex, slot);
+    mode = Item.SetData(ItemIndex, 44);
+  end
+  if mode == 1 then
+    local Time = Item.SetData(ItemIndex, 45);
+    return Time - os.time();
+  end
+  return nil;
+end
