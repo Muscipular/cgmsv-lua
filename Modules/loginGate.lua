eea69de6cc43c4a4404356bb990ed0a8d8da26b4
Muscipular <m@ljw.im>
2021-10-29 11:11:00 +0800

    [#] 原地登陆模块修复bug

--- Modules/loginGate.lua
+++ Modules/loginGate.lua
@@ -1,8 +1,17 @@
 ---原地登陆
 local LoginModule = ModuleBase:createModule('loginGate')
 local LoginPoints = {};
--- 法兰城登陆点设置 LoginPoint = 1
-LoginPoints[1] = {
+-- 出生点
+LoginPoints[-1] = {
+  { 0, 1530, 15, 6 },
+  { 0, 1530, 15, 6 },
+  { 0, 1530, 15, 6 },
+  { 0, 1530, 15, 6 },
+  { 0, 1530, 15, 6 },
+  { 0, 1530, 15, 6 },
+}
+-- 法兰城登陆点设置 LoginPoint = 0
+LoginPoints[0] = {
   { 0, 1000, 233, 78 },
   { 0, 1000, 233, 78 },
   { 0, 1000, 233, 78 },
@@ -10,22 +19,25 @@ LoginPoints[1] = {
   { 0, 1000, 233, 78 },
   { 0, 1000, 233, 78 },
 };
--- 阿凯鲁法村登录点设置 LoginPoint = 2
-LoginPoints[2] = {
+-- 阿凯鲁法村登录点设置 LoginPoint = 1
+LoginPoints[1] = {
   { 0, 33200, 99, 165 },
 };
--- 哥拉尔登陆点设置 LoginPoint = 3
-LoginPoints[3] = {
+-- 哥拉尔登陆点设置 LoginPoint = 2
+LoginPoints[2] = {
   { 0, 43100, 120, 107 },
 };
--- 新城登陆点设置 LoginPoint = 4
-LoginPoints[4] = {
+-- 新城登陆点设置 LoginPoint = 3
+LoginPoints[3] = {
   { 0, 59520, 140, 105 },
 };
 
 function LoginModule:GetLoginPointEvent(charIndex, mapID, floorID, x, y)
+  if Char.EndEvent(charIndex, 0) ~= 1 then
+    return
+  end
   local json = Field.Get(charIndex, 'LastLogoutPoint');
-  local ret, lastPoint;
+  local ret, lastPoint = nil, nil;
   if json and json ~= 'null' then
     ret, lastPoint = pcall(JSON.decode, json)
     if ret and lastPoint then
@@ -38,8 +50,9 @@ function LoginModule:GetLoginPointEvent(charIndex, mapID, floorID, x, y)
       end
     end
   end
+  --self:logDebug('LoginPoint', Char.GetData(charIndex, CONST.CHAR_登陆点), table.unpack(lastPoint or {}))
   if lastPoint == nil then
-    lastPoint = LoginPoints[Char.GetData(charIndex, CONST.CHAR_登陆点)] or LoginPoints[1];
+    lastPoint = LoginPoints[Char.GetData(charIndex, CONST.CHAR_登陆点)] or LoginPoints[0];
     lastPoint = lastPoint[math.random(1, #lastPoint)]
   end
   Field.Set(charIndex, 'LastLogoutPoint', 'null');
