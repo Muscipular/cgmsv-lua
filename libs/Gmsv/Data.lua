c2f6c43bd02829cce0b853493e61b67bc8dc08ae
Muscipular <m@ljw.im>
2021-11-05 19:03:32 +0800

    [+] Data.GetEnemyBaseIdByEnemyId [+] Data.GetEnemyBaseIndexByEnemyId

--- libs/Gmsv/Data.lua
+++ libs/Gmsv/Data.lua
@@ -140,3 +140,23 @@ function Data.GetMessage(msgId)
   end
   return ffi.string(MSG_getMessage(msgId));
 end
+
+function Data.GetEnemyBaseIdByEnemyId(enemyId)
+  enemyId = tonumber(enemyId)
+  if enemyId == nil then
+    return nil;
+  end
+  local index = Data.EnemyGetDataIndex(enemyId)
+  if index < 0 then
+    return nil;
+  end
+  return Data.EnemyGetData(index, CONST.DATA_ENEMY_TEMPNO)
+end
+
+function Data.GetEnemyBaseIndexByEnemyId(enemyId)
+  local id = Data.GetEnemyBaseIdByEnemyId(enemyId)
+  if id == nil or id < 0 then
+    return id;
+  end
+  return Data.EnemyBaseGetDataIndex(id);
+end
