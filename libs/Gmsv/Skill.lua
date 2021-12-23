83a20361b26c9ff7e3931816d0b889b4f0b8c244
Muscipular <m@ljw.im>
2021-12-23 19:18:23 +0800

    [#] 15级技能自定义-96%

--- libs/Gmsv/Skill.lua
+++ libs/Gmsv/Skill.lua
@@ -3,7 +3,7 @@ _G.Skill = _G.Skill or {}
 local getSkillExpDataInt;
 local expTable = {}
 
-local MAX_SKill_Lv = 15;
+local MAX_SKill_Lv = 15; --最大127 理论上？
 
 local function hookGetSkillExpDataInt(index, lvIndex)
   --print(index, lvIndex);
@@ -16,6 +16,26 @@ end
 getSkillExpDataInt = ffi.hook.new('int (__cdecl*)(int a1, int a2)', hookGetSkillExpDataInt, 0x004F5420, 6);
 local getSkillExpIndex = ffi.cast('int (__cdecl*)(int a)', 0x004F5490);
 
+
+--[[
+--设置扩展技能经验表例子
+Skill.SetExpForLv(2, 11, 3300);
+Skill.SetExpForLv(2, 12, 3400);
+Skill.SetExpForLv(2, 13, 3500);
+Skill.SetExpForLv(2, 14, 3500);
+Skill.SetExpForLv(2, 15, 3600);
+Skill.SetExpForLv(2, 16, 3601);
+Skill.SetExpForLv(2, 17, 3602);
+Skill.SetExpForLv(2, 18, 3603);
+Skill.SetExpForLv(2, 19, 3604);
+Skill.SetExpForLv(2, 20, 3605);
+Skill.SetExpForLv(2, 21, 3606);
+Skill.SetExpForLv(2, 22, 3607);
+]]
+---设置扩展技能经验表
+---@param expId number
+---@param lv number
+---@param exp number 
 function Skill.SetExpForLv(expId, lv, exp)
   if expId < 0 then
     return -1;
@@ -38,15 +58,7 @@ end
 ffi.patch(0x00442533 + 2, { MAX_SKill_Lv });
 ffi.patch(0x00442811 + 2, { MAX_SKill_Lv });
 ffi.patch(0x004F9426 + 3, { MAX_SKill_Lv - 1 });
---ffi.patch(0x0042A69D + 3, { 0x0F });
---ffi.patch(0x0042A844 + 3, { 0x0F });
 ffi.patch(0x0044222F + 6, { MAX_SKill_Lv - 1 });
---ffi.patch(0x00430A8F + 6, { 0x10 });
-----expand stack size
---ffi.patch(0x00430EBE + 2, { 0x0C, 0x02, 0x00, 0x00 });
---ffi.patch(0x0043090B + 2, { 0x0C, 0x02, 0x00, 0x00 });
-
---TODO HOOK
 
 local getSkillIndex = ffi.cast('uint32_t (__cdecl*)(int a)', 0x004F4AB0);
 local getTechId = ffi.cast('int (__cdecl*)(const char *file, int lineNo, uint32_t charPtr, int slot, uint32_t lv)', 0x0042A590);
@@ -232,8 +244,3 @@ regGlobalEvent('DropEvent', function(charIndex)
   charSkillData[charIndex] = nil;
 end)
 
-Skill.SetExpForLv(2, 11, 3300);
-Skill.SetExpForLv(2, 12, 3400);
-Skill.SetExpForLv(2, 13, 3500);
-Skill.SetExpForLv(2, 14, 3500);
-Skill.SetExpForLv(2, 15, 3600);
