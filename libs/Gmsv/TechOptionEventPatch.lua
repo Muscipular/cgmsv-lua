1c8295238321750dec4319c3a0b57ff0bd34f897
Muscipular <m@ljw.im>
2022-01-25 17:35:30 +0800

    [#] 修复TechOptionEvent参数问题 [#] 修复TechOptionEvent对魔法的AR参数支持 [#] 修复魔法套装效果对AR的支持

--- libs/Gmsv/TechOptionEventPatch.lua
+++ libs/Gmsv/TechOptionEventPatch.lua
@@ -6,24 +6,22 @@ ffi.patch(0x00576E26 + 1, { 0x15, 0xD5 })
 
 local GetSuitsetTechOpt = ffi.cast('int (__cdecl*)(uint32_t charAddr, int techId, const char *type, int val)', 0x00478710);
 ffi.hook.inlineHook('int (__cdecl *)(const char*, uint32_t, uint32_t)', function(techData, charPtr, valPtr)
-  local s = ffi.C.strstr(techData, "AR:");
+  local s = ffi.string(techData);
   local v = 0;
-  print(ffi.string(s), v);
-  if ffi.cast('uint32_t', s) > 0 then
-    v = string.match(ffi.string(s + 3), "^(%d+)")
-    print(ffi.string(s + 3), v);
-    if v == nil then
-      v = 0
-    else
-      v = tonumber(v) or 0;
-    end
+  v = string.match(s, "AR:(%d+)")
+  --print(s, v);
+  if v == nil then
+    v = 0
+  else
+    v = tonumber(v) or 0;
   end
   local charIndex = ffi.readMemoryInt32(charPtr + 4);
   local techId = Char.GetData(charIndex, CONST.CHAR_BattleCom3) or -1;
   local techIndex = Tech.GetTechIndex(techId);
-  print(charIndex, charPtr, techId, v)
-  v = GetSuitsetTechOpt(charPtr, techIndex, "AR:", v);
-  print(charIndex, charPtr, techId, v)
+  --print(charIndex, charPtr, techId, v);
+  --printAsHex(valPtr, tostring(ffi.readMemoryInt32(valPtr)));
+  v = GetSuitsetTechOpt(charPtr, techIndex, ffi.cast('void*', 0x0061B9CA), v);
+  --print(charIndex, charPtr, techId, v)
   ffi.setMemoryInt32(valPtr, v);
   return 0;
 end, 0x00498D52, 0x00498D78 - 0x00498D52, {
