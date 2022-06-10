a400c4501655357c11d34b2216f1190760e96be8
Muscipular <m@ljw.im>
2022-06-11 01:20:16 +0800

    [#] 移动Char.UpCharStatus到c++ [#] 修复连战bug [#] 修复因果伤害问题

--- libs/Gmsv/Char.lua
+++ libs/Gmsv/Char.lua
@@ -397,13 +397,13 @@ function Char.RemovePoolItem(charIndex, slot)
   return 0;
 end
 
-local UpCharStatus = ffi.cast('int (__cdecl*)(uint32_t a1, int a3)', 0x00432FE0);
-
-function Char.UpCharStatus(charIndex)
-  local ptr = Char.GetCharPointer(charIndex)
-  if ptr < Addresses.CharaTablePTR then
-    return -1;
-  end
-  UpCharStatus(ptr, 1)
-  return 0;
-end
+--local UpCharStatus = ffi.cast('int (__cdecl*)(uint32_t a1, int a3)', 0x00432FE0);
+--
+--function Char.UpCharStatus(charIndex)
+--  local ptr = Char.GetCharPointer(charIndex)
+--  if ptr < Addresses.CharaTablePTR then
+--    return -1;
+--  end
+--  UpCharStatus(ptr, 1)
+--  return 0;
+--end
