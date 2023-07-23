9d05bb61318a17a687ff2a3e345c9e01a06eb30b
Muscipular <m@ljw.im>
2023-07-24 00:07:23 +0800

    [#] vscode添加常量支持

---
+++ .vscode/Const.lua
@@ -0,0 +1,26 @@
+---@class diff
+---@field start  integer # The number of bytes at the beginning of the replacement
+---@field finish integer # The number of bytes at the end of the replacement
+---@field text   string  # What to replace
+
+---@param  uri  string # The uri of file
+---@param  text string # The content of file
+---@return nil|diff[]
+function OnSetText(uri, text)
+    local diffs = {}
+-- print(text);
+    for start, a, finish in text:gmatch("()(%%[^%% ]*_[^%% ]*%%)()") do
+        print(start, a, finish);
+        diffs[#diffs+1] = {
+            start  = start,
+            finish = finish - 1,
+            text   = "0",
+        }
+    end
+
+    if #diffs == 0 then
+        return nil
+    end
+
+    return diffs
+end
