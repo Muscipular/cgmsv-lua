8da07a07e73c0f3225c0bf13dc5e29fd682cd1c3
Ralu <->
2023-12-07 18:47:53 +0800

    [#] update docs

---
+++ docs/Setup.lua
@@ -0,0 +1,12 @@
+---@meta _
+
+---获取职业/职系的声望上限
+---@param job number 职业/职系Id
+---@return number @声望上限
+function Setup.GetJobFameLimit(job) end
+
+---设置职业/职系的声望上限
+---@param job number 职业/职系Id
+---@param limit number 声望上限
+---@return number 成功返回1
+function Setup.SetJobFameLimit(job, limit) end
