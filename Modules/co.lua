b10b1a4bfe78f2123814d6808591c63d7a9fc419
Muscipular <m@ljw.im>
2025-06-05 00:50:54 +0800

    [#] update co

--- Modules/co.lua
+++ Modules/co.lua
@@ -8,26 +8,35 @@ function CoModule:onLoad()
     _G.loggerLevel = 99;
     self:SetLogLevel(99);
     self:logInfo('load')
-    self.co = self:NPC_CreateCo("CO!", 104863, { map = 1000, mapType = 0, x = 229, y = 88, direction = 6 },
-        function(co, npc, player, msg, color, size)
-            self:logDebug("start co", co, npc, player, msg, color, size)
-            local n = SQL.QueryEx("select X from cgmsv.tbl_character where CdKey = ? and RegistNumber = ?;",
-                Char.GetData(player, CONST.CHAR_CDK),
-                Char.GetData(player, CONST.对象_RegistNumber));
-            npc, player, seqno, select, data = co:next(player, npc, CONST.窗口_信息框, CONST.BUTTON_是否, 0,
-                self:NPC_buildSelectionText("Text", { "A", "B", n.rows[1].X }));
-            self:logDebug("co1", co, npc, player, seqno, select, data)
-            n = SQL.QueryEx("select X from cgmsv.tbl_character where CdKey = ? and RegistNumber = ?;",
-            Char.GetData(player, CONST.CHAR_CDK),
-            Char.GetData(player, CONST.对象_RegistNumber));
-            npc, player, seqno, select, data = co:next(player, npc, CONST.窗口_选择框, CONST.BUTTON_是否, 0,
-                self:NPC_buildSelectionText("Text2", { "C", "D", n.rows[1].X }));
-            self:logDebug("co2", co, npc, player, seqno, select, data)
+    self.co = self:NPC_CreateCo("CO!", 104863,
+        { map = 1000, mapType = 0, x = 229, y = 88, direction = 6 },
+        Func.bind(self.CO_Func, self));
+end
 
-            npc, player, seqno, select, data = co:next(player, npc, CONST.窗口_选择框, CONST.BUTTON_是否, 0,
-                self:NPC_buildSelectionText("Text3", { "E", "F" }));
-            self:logDebug("co3", co, npc, player, seqno, select, data)
-        end)
+---@param co CO
+---@param npc number
+---@param player number
+---@param msg string
+---@param color number
+---@param size number
+function CoModule:CO_Func(co, npc, player, msg, color, size)
+    self:logDebug("start co", co, npc, player, msg, color, size)
+    local n = SQL.QueryEx("select X from cgmsv.tbl_character where CdKey = ? and RegistNumber = ?;",
+        Char.GetData(player, CONST.CHAR_CDK),
+        Char.GetData(player, CONST.对象_RegistNumber));
+    npc, player, seqno, select, data = co:next(player, npc, CONST.窗口_信息框, CONST.BUTTON_是否, 0,
+        self:NPC_buildSelectionText("Text", { "A", "B", n.rows[1].X }));
+    self:logDebug("co1", co, npc, player, seqno, select, data)
+    n = SQL.QueryEx("select X from cgmsv.tbl_character where CdKey = ? and RegistNumber = ?;",
+        Char.GetData(player, CONST.CHAR_CDK),
+        Char.GetData(player, CONST.对象_RegistNumber));
+    npc, player, seqno, select, data = co:next(player, npc, CONST.窗口_选择框, CONST.BUTTON_是否, 0,
+        self:NPC_buildSelectionText("Text2", { "C", "D", n.rows[1].X }));
+    self:logDebug("co2", co, npc, player, seqno, select, data)
+
+    npc, player, seqno, select, data = co:next(player, npc, CONST.窗口_选择框, CONST.BUTTON_是否, 0,
+        self:NPC_buildSelectionText("Text3", { "E", "F" }));
+    self:logDebug("co3", co, npc, player, seqno, select, data)
 end
 
 --- 卸载模块钩子
