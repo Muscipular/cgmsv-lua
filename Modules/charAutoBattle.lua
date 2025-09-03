f1fe02a8bec589eeca7f535925294f160d49e487
Muscipular <m@ljw.im>
2025-09-03 19:04:33 +0800

    [#] lua自动战斗适配25.9h,无需cg支持

--- Modules/charAutoBattle.lua
+++ Modules/charAutoBattle.lua
@@ -1,24 +1,48 @@
 ---模块类
+---@class Module: ModuleType
 local Module = ModuleBase:createModule('charAutoBattle')
+
+function Module:talked(player, _msg)
+  if _msg == '/autobattle on' then
+    Char.SetTempData(player, "__SKIP_BATTLE_ACTION", 1);
+    NLG.SystemMessage(player, "自动战斗开启");
+    return 0;
+  elseif _msg == '/autobattle off' then
+    Char.SetTempData(player, "__SKIP_BATTLE_ACTION", nil);
+    NLG.SystemMessage(player, "自动战斗关闭");
+    return 0;
+  end
+end
+
+function Module:battleAction(battleIndex, turn)
+  for i = 1, 5 do
+    local ch = Battle.GetPlayIndex(battleIndex, i - 1);
+    if ch >= 0 then
+      if Char.GetData(ch, CONST.CHAR_类型) == CONST.对象类型_人 then
+        if (Char.GetTempData(ch, "__SKIP_BATTLE_ACTION") == 1 && Battle.IsWaitingCommand(ch) == 1) then
+          Battle.ActionSelect(ch, CONST.BATTLE_COM.BATTLE_COM_ATTACK, 10, -1);
+        end
+        goto CONTINUE;
+      end
+    end
+    local ch2 = Battle.GetPlayIndex(battleIndex, i - 1 + 5);
+    if ch2 >= 0 then
+      if ch < 0 then
+        ch = ch2;
+      end
+      if (Char.GetTempData(ch2, "__SKIP_BATTLE_ACTION") == 1 && Battle.IsWaitingCommand(ch) == 1) then
+        Battle.ActionSelect(ch, CONST.BATTLE_COM.BATTLE_COM_ATTACK, 10, -1);
+      end
+    end
+    ::CONTINUE::
+  end
+end
 
 --- 加载模块钩子
 function Module:onLoad()
   self:logInfo('load')
-  self:regCallback("ProtocolOnRecv", function(fd, head, list)
-    self:logDebugF("AutoBattle %d %s", fd, head);
-    local charIndex = tonumber(Protocol.GetCharIndexFromFd(fd));
-    Protocol.Send(charIndex, "AutoBattle");
-    local p = Battle.GetSlot(Battle.GetCurrentBattle(charIndex), charIndex);
-    p = math.fmod(p + 5, 10);
-    p = Battle.GetPlayer(Battle.GetCurrentBattle(charIndex), p) or -1;
-    self:logDebugF("AutoBattle %d %s %s %s", fd, head, charIndex, p);
-    Battle.ActionSelect(charIndex, CONST.BATTLE_COM.BATTLE_COM_ATTACK, CONST.BATTLE_COM_TARGETS.SINGLE.SIDE_1.POS_0, -1);
-    if p >= 0 then
-      Battle.ActionSelect(p, CONST.BATTLE_COM.BATTLE_COM_ATTACK, CONST.BATTLE_COM_TARGETS.SINGLE.SIDE_1.POS_0, -1);
-    else
-      Battle.ActionSelect(charIndex, CONST.BATTLE_COM.BATTLE_COM_ATTACK, CONST.BATTLE_COM_TARGETS.SINGLE.SIDE_1.POS_0, -1);
-    end
-  end, "AutoBattle")
+  self:regCallback('TalkEvent', Func.bind(self.talked, self))
+  self:regCallback('BeforeBattleTurnEvent', Func.bind(self.battleAction, self))
 end
 
 --- 卸载模块钩子
